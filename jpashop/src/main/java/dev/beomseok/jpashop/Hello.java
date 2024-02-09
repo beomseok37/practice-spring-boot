e7415764737ed2fb8f760780534b021704ea0df9
beomseok <bmtosss@gmail.com>
2024-02-09 12:29:04 +0900

    섹션 1 수강

---
+++ jpashop/src/main/java/dev/beomseok/jpashop/Hello.java
@@ -0,0 +1,9 @@
+package dev.beomseok.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String name;
+}
---
+++ jpashop/src/test/java/dev/beomseok/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package dev.beomseok.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void testMember(){
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long id = memberRepository.save(member);
+        Member findMember = memberRepository.find(id);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
