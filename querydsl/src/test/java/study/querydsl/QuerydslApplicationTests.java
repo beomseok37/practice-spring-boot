841b129071b335bff97a1e238b5fc9729cd5e500
beomseok <bmtosss@gmail.com>
2024-02-17 16:30:46 +0900

    섹션 5 수강

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,12 +1,7 @@
 package study.querydsl;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-import study.querydsl.entity.QHello;
 
 @SpringBootTest
 class QuerydslApplicationTests {
