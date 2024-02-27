56b86996867fd1852b9e320d1caba3682f2d4036
beomseok <bmtosss@gmail.com>
2024-02-27 12:44:10 +0900

    엔티티 생성

---
+++ library-app/src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -0,0 +1,25 @@
+package com.group.libraryapp.domain.book;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Book(){}
+
+    public Book(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
